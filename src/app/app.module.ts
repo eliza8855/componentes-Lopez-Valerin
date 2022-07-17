5f07344be6c1985aecedfe01e3569f4ee607f85c
Elizabeth Benilde Lopez Valerin <elopeval@emeal.nttdata.com>
2022-07-16 23:41:42 -0500

    se crean 3 componentes basicos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ToolbarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/content/content.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  cards = [ 'card 1', 'card 2', 'card 3', 'card 4']
+}
